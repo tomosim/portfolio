3b005bd76d16e705278858c57feef3a080b0a5cc
Tom <TomSimmons@Toms-MBP-3.fritz.box>
2021-02-02 12:10:55 +0100

    assemble components so far

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import "./App.css";
+import { Panel } from "./Styled/Panel";
+import { Panels } from "./Styled/Panels";
+import { useState } from "react";
+import { BackgroundGrad } from "./Styled/BackgroundGrad";
 
 function App() {
-  return <div></div>;
+  const [activePanel, setActivePanel] = useState(0);
+  return (
+    <BackgroundGrad>
+      <Panels>
+        <Panel
+          className={activePanel === 0 && "active"}
+          onClick={() => setActivePanel(0)}
+        ></Panel>
+        <Panel
+          className={activePanel === 1 && "active"}
+          onClick={() => setActivePanel(1)}
+        ></Panel>
+        <Panel
+          className={activePanel === 2 && "active"}
+          onClick={() => setActivePanel(2)}
+        ></Panel>
+        <Panel
+          className={activePanel === 3 && "active"}
+          onClick={() => setActivePanel(3)}
+        ></Panel>
+        <Panel
+          className={activePanel === 4 && "active"}
+          onClick={() => setActivePanel(4)}
+        ></Panel>
+      </Panels>
+    </BackgroundGrad>
+  );
 }
 
 export default App;
