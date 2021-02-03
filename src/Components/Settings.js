9841a41315b0a164264b2cf2f1b9595b5fcfc2e4
Tom <TomSimmons@Toms-MBP-3.fritz.box>
2021-02-03 14:46:32 +0100

    add toggle switch for nightmode

--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SliderWrapper from "./SliderWrapper";
+import Toggle from "react-styled-toggle";
 
 const Settings = ({
   setColorOne,
@@ -11,22 +12,32 @@ const Settings = ({
 }) => {
   return (
     <>
+      <Toggle
+        checked={nightMode ? false : true}
+        labelLeft="Night Mode"
+        labelRight="Day Mode"
+        height="28"
+        width="50"
+        sliderHeight="22"
+        sliderWidth="22"
+        translate="20"
+        backgroundColorChecked="#303030"
+        onChange={() => {
+          setNightMode(!nightMode);
+        }}
+      />
+      <h3>Colour One</h3>
       <SliderWrapper
-        text="Colour One"
         setColour={setColorOne}
         colour={colorOne}
         nightMode={nightMode}
       ></SliderWrapper>
+      <h3>Colour Two</h3>
       <SliderWrapper
-        text="Colour Two"
         setColour={setColorTwo}
         colour={colorTwo}
         nightMode={nightMode}
       ></SliderWrapper>
-      <h3>Night Mode</h3>
-      <button onClick={() => setNightMode(!nightMode)}>
-        {nightMode ? "Off" : "On"}
-      </button>
     </>
   );
 };
--- src/Components/SliderWrapper.js
+++ src/Components/SliderWrapper.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Slider from "../Styled/Slider";
+import { StyledSliderWrapper } from "../Styled/StyledSliderWrapper";
 
 const SliderWrapper = ({ text, setColour, colour, nightMode }) => {
   const [redValue, setRedValue] = useState(colour[0]);
@@ -11,56 +12,53 @@ const SliderWrapper = ({ text, setColour, colour, nightMode }) => {
   }, [redValue, greenValue, blueValue, setColour]);
 
   return (
-    <div>
-      <h3>{text}</h3>
-      <div>
-        <Slider
-          colour={`rgb(${redValue}, ${(10 / 255) * redValue}, ${
-            (10 / 255) * redValue
-          })`}
-          borderColour="rgb(255, 100, 100)"
-          type="range"
-          min="1"
-          max="255"
-          id="red"
-          value={redValue}
-          nightMode={nightMode}
-          onChange={(e) => {
-            setRedValue(e.target.value);
-          }}
-        />
-        <Slider
-          colour={`rgb( ${(10 / 255) * greenValue}, ${greenValue}, ${
-            (10 / 255) * greenValue
-          })`}
-          borderColour="rgb(100, 255, 100)"
-          type="range"
-          min="1"
-          max="255"
-          id="green"
-          nightMode={nightMode}
-          value={greenValue}
-          onChange={(e) => {
-            setGreenValue(e.target.value);
-          }}
-        />
-        <Slider
-          colour={`rgb( ${(10 / 255) * blueValue}, ${
-            (10 / 255) * blueValue
-          }, ${blueValue})`}
-          borderColour="rgb(100, 100, 255)"
-          type="range"
-          min="1"
-          max="255"
-          id="blue"
-          nightMode={nightMode}
-          value={blueValue}
-          onChange={(e) => {
-            setBlueValue(e.target.value);
-          }}
-        />
-      </div>
-    </div>
+    <StyledSliderWrapper>
+      <Slider
+        colour={`rgb(${redValue}, ${(10 / 255) * redValue}, ${
+          (10 / 255) * redValue
+        })`}
+        borderColour="rgb(255, 100, 100)"
+        type="range"
+        min="1"
+        max="255"
+        id="red"
+        value={redValue}
+        nightMode={nightMode}
+        onChange={(e) => {
+          setRedValue(e.target.value);
+        }}
+      />
+      <Slider
+        colour={`rgb( ${(10 / 255) * greenValue}, ${greenValue}, ${
+          (10 / 255) * greenValue
+        })`}
+        borderColour="rgb(100, 255, 100)"
+        type="range"
+        min="1"
+        max="255"
+        id="green"
+        nightMode={nightMode}
+        value={greenValue}
+        onChange={(e) => {
+          setGreenValue(e.target.value);
+        }}
+      />
+      <Slider
+        colour={`rgb( ${(10 / 255) * blueValue}, ${
+          (10 / 255) * blueValue
+        }, ${blueValue})`}
+        borderColour="rgb(100, 100, 255)"
+        type="range"
+        min="1"
+        max="255"
+        id="blue"
+        nightMode={nightMode}
+        value={blueValue}
+        onChange={(e) => {
+          setBlueValue(e.target.value);
+        }}
+      />
+    </StyledSliderWrapper>
   );
 };
 
